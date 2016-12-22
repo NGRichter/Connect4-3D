0304cbd4c7e89eb472b102391ca6efb085624d26
NGRichter <n.g.richter@student.utwente.nl>
2016-12-22 23:44:23 +0100

    Added ~1200 colours

--- Connected4-3D/src/connected/game/HumanPlayer.java
+++ Connected4-3D/src/connected/game/HumanPlayer.java
@@ -1,5 +1,16 @@
 package connected.game;
 
-public class HumanPlayer {
+public class HumanPlayer extends Player {
+
+	public HumanPlayer(String name, Colour colour) {
+		super(name, colour);
+	}
+
+	@Override
+	public int determineMove(Board board) {
+        String prompt = "> " + getName() + " (" + getColour().name() + ")"
+                + ", what is your choice? ";
+        return 0;
+	}
 
 }
