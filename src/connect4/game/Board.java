b97c2a9d713952351760b7273ceeabf49b39a954
NGRichter <n.g.richter@student.utwente.nl>
2017-01-04 13:07:19 +0100

    Updated

--- src/connect4/game/Board.java
+++ src/connect4/game/Board.java
@@ -1,16 +1,56 @@
 package connect4.game;
 
+import connect4.exceptions.OutsidePlayingBoardException;
+
 public class Board {
 	
 	private Colour[][][] fields;
+	public final int DIMX;
+	public final int DIMY;
+	public final int DIMZ;
 	
 	
 	public Board(int x, int y, int z) {
 		fields = new Colour[x][y][z];
+		DIMX = x;
+		DIMY = y;
+		DIMZ = z;
 	}
 
 	public void setField(int choice, Colour colour) {
 		
+		
+	}
+	
+	public int getDimX() {
+		return DIMX;
+	}
+	
+	public int getDimY() {
+		return DIMY;
+	}
+	
+	public int getDimZ() {
+		return DIMZ;
+	}
+	
+	public Colour getField(int choice) {
+		Colour colour;
+		
+		colour = fields[x][y][z];
+		
+		return colour;
+	}
+	
+	public int[] index(int choice) throws OutsidePlayingBoardException {
+		int x = choice % DIMX;
+		int y = (choice / DIMX) % DIMY;
+		int z = choice / (DIMX * DIMY);
+		if (z > DIMZ) {
+			throw new OutsidePlayingBoardException();
+		}
+		int[] array = {x,y,z};
+		return array;
 		
 	}
 	
