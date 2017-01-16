278b702b6d0084f32651e8990976107b7e2fc484
Julian Flapper <jhjflapper@gmail.com>
2017-01-16 12:33:39 +0100

    Board toString beginnings

--- src/connect4/game/Board.java
+++ src/connect4/game/Board.java
@@ -82,6 +82,18 @@ public class Board {
 		return array;
 		
 	}
-	
+
+
+	public String toString{
+        String leftAlignFormat = "| %-15s | %-4d |%n";
+
+        System.out.format("+-----------------+------+%n");
+        System.out.format("| Column name     | ID   |%n");
+        System.out.format("+-----------------+------+%n");
+        for (int i = 0; i < 5; i++) {
+            System.out.format(leftAlignFormat, "some data" + i, i * i);
+        }
+        System.out.format("+-----------------+------+%n");
+    }
 	
 }
