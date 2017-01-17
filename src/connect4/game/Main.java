9513697c0fc92d59e1ab11bb4bdcea26cf1a007f
NGRichter <n.g.richter@student.utwente.nl>
2017-01-17 22:28:16 +0100

    Added Minimax algorythm with Alpha-Beta pruning

--- src/connect4/game/Main.java
+++ src/connect4/game/Main.java
@@ -7,10 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		Strategy medium = new Medium();
 		Strategy hard = new Hard();
-		Player player1 = new ComputerPlayer(args[0], Colour.AERO, medium);
-		Player player2 = new ComputerPlayer(args[1], Colour.ACID_GREEN, hard);
+		Player player1 = new HumanPlayer(args[0], Colour.AERO);
+		Player player2 = new HumanPlayer(args[1], Colour.ACID_GREEN);
 		Player[] players = {player1, player2};
-		Board board = new Board(6, 6, 6);
+		Board board = new Board(4, 4, 4);
 		Game game = new Game(board, players, 4);
 		game.start();
 	}
