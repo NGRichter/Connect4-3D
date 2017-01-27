91c1bdf059968acd1fc32c30a546613528c8f424
Julian Flapper <jhjflapper@gmail.com>
2017-01-27 10:18:28 +0100

    sdsdsdas

--- src/connect4/ui/Gui.java
+++ src/connect4/ui/Gui.java
@@ -17,10 +17,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import sun.plugin.javascript.navig.Anchor;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -33,18 +33,25 @@ public class Gui extends Application implements GameView, Initializable {
 
     private Stage window;
     private Client client;
-    private int HEIGHT = 640;
-    private int WIDTH = 480;
+
+    //Application settings
+    private int HEIGHT = 1280;
+    private int WIDTH = 720;
+    private boolean RESIZABLE = false;
 
     @FXML private Button connectButton, joinButton;
     @FXML private TextField ipField, portField, usernameField;
     @FXML private PasswordField passwordField;
     @FXML private HBox connectBox, joinBox;
+    @FXML  private Parent root, game;
+    private Scene scene;
+    @FXML private BorderPane rootPane;
 
     @Override
     public void init() {
         client = new Client(this);
     }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         client = new Client(this);
@@ -55,21 +62,41 @@ public class Gui extends Application implements GameView, Initializable {
 	public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         window.setTitle("Connect4-3D GUI client");
+        window.setResizable(RESIZABLE);
 
-        Parent main = FXMLLoader.load(getClass().getResource("fxml\\multiplayer.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml\\connect.fxml"));
+        loader.setController(this);
+        BorderPane mainPane = loader.load();
 
-        Scene scene = new Scene(main, HEIGHT, WIDTH);
+        scene = new Scene(mainPane);
+
         primaryStage.setScene(scene);
         primaryStage.show();
-	}
+}
+
 
 
 	public void connect() {
-        try {
+        if (usernameField.getText().trim().isEmpty()){
+            showError("Please specify a username.");
+        } else try {
             InetAddress address = InetAddress.getByName(ipField.getText());
             int port = Integer.parseInt(portField.getText());
             client.connectServer(port, address);
             writeServer("Join " + usernameField.getText() + " chat security challenge leaderboard");
+
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml\\game.fxml"));
+            loader.setController(this);
+            try {
+                BorderPane gamePane = loader.load();
+                scene = new Scene(gamePane);
+                window.setScene(scene);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
         } catch (UnknownHostException e) {
             showError("ip-address invalid.");
         } catch (NumberFormatException e) {
@@ -77,11 +104,6 @@ public class Gui extends Application implements GameView, Initializable {
         } catch (IOException e) {
             showError("cannot connect to server");
         }
-    }
-
-    public void join() {
-
-        window.setScene(null);
     }
 
 	@Override
