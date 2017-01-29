f2f5e087ef2bacfb0715b831b3f4239778d617b5
Julian Flapper <jhjflapper@gmail.com>
2017-01-29 13:53:18 +0100

    dsdsdsa

--- src/connect4/ui/Gui.java
+++ src/connect4/ui/Gui.java
@@ -1,11 +1,14 @@
 package connect4.ui;
 
+import com.sun.org.apache.xpath.internal.functions.FuncFalse;
 import connect4.game.Board;
 import connect4.game.Game;
 import connect4.game.GameView;
 import connect4.game.Player;
 import connect4.network.server.Buffer;
 import connect4.network.client.Client;
+import javafx.animation.Animation;
+import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -13,13 +16,19 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.geometry.Point3D;
+import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.DrawMode;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import sun.font.FontManagerNativeLibrary;
 import sun.plugin.javascript.navig.Anchor;
 
@@ -40,6 +49,10 @@ public class Gui extends Application implements GameView, Initializable {
     private int WIDTH = 720;
     private boolean RESIZABLE = false;
 
+    //3D Scene settings
+    private int SCENE3DHEIGHT = 675;
+    private int SCENE3DWIDTH = 970;
+
     private Scene scene;
     @FXML private Button connectButton, joinButton, moveButton, readyButton;
     @FXML private TextField ipField, portField, usernameField, chatField, xField,
@@ -52,7 +65,10 @@ public class Gui extends Application implements GameView, Initializable {
     @FXML private VBox readyBox;
     @FXML private Parent root, game;
     @FXML private BorderPane gamePane;
-    @FXML private Pane connectPane, lobbyPane;
+    @FXML private StackPane centerPane;
+    @FXML private Pane connectPane, lobbyPane, boardPane;
+    @FXML private PerspectiveCamera camera;
+    @FXML private Group boardGroup;
 
     @Override
     public void init() {
@@ -67,6 +83,54 @@ public class Gui extends Application implements GameView, Initializable {
         gamePane.toBack();
         connectPane.setVisible(true);
         connectPane.toFront();
+
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+
+
+        Box shape1 = new Box();
+        shape1.setHeight(20.0);
+        shape1.setWidth(20.0);
+        shape1.setDepth(20.0);
+
+        shape1.setMaterial(redMaterial);
+        shape1.setDrawMode(DrawMode.FILL);
+
+        Box shape2 = new Box();
+        shape2.setHeight(20.0);
+        shape2.setWidth(20.0);
+        shape2.setDepth(20.0);
+
+        shape2.setMaterial(redMaterial);
+        shape2.setDrawMode(DrawMode.FILL);
+        shape2.setTranslateX(50.0);
+
+        boardGroup = new Group(shape1, shape2);
+        boardGroup.setTranslateX(SCENE3DWIDTH/2);
+        boardGroup.setTranslateY(SCENE3DHEIGHT/2);
+
+        // Add a Rotation animation to the camera
+        RotateTransition rt = new RotateTransition(Duration.seconds(2), boardGroup);
+        rt.setCycleCount(Animation.INDEFINITE);
+        rt.setFromAngle(0);
+        rt.setToAngle(360);
+        rt.setAutoReverse(false);
+        rt.setAxis(Rotate.Y_AXIS);
+        rt.play();
+
+
+        boardPane.getChildren().add(createScene3D(boardGroup));
+    }
+
+    public SubScene createScene3D(Group group){
+        SubScene scene3d = new SubScene(group, SCENE3DWIDTH , SCENE3DHEIGHT);
+        scene3d.setFill(Color.rgb(10, 10, 40));
+        camera = new PerspectiveCamera();
+        camera.setRotationAxis(Rotate.X_AXIS);
+        camera.setRotate(-30.0);
+        scene3d.setCamera(camera);
+        return scene3d;
     }
 
 
@@ -82,8 +146,7 @@ public class Gui extends Application implements GameView, Initializable {
 
         primaryStage.setScene(scene);
         primaryStage.show();
-
-}
+    }
 
     public void connect() {
         if (usernameField.getText().trim().isEmpty()){
